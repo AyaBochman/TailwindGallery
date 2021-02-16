ff158cf2eec2dc0f35af875c6e551d7969ebbe9d
aya <aya.bochman@gmail.com>
2021-02-16 18:06:47 +0200

    cards in process, need to handle data

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
 
 const Index = ({ data }) => (
     <div>
+        <title>Aya Bochman</title>
         <Main data={data} />
     </div>
 )
@@ -11,17 +12,23 @@ const Index = ({ data }) => (
 export async function getStaticProps() {
     var Airtable = require('airtable');
     var apiKey = serverRuntimeConfig.API_KEY;
-    var base = new Airtable({ apiKey }).base('appKy6SwWCmfyQOWS');
-    const table = base('Artists');
+    var base = new Airtable({ apiKey }).base('appDAc9CXCfSiBuV8');
+    const table = base('Content');
     const records = await table.select({
-        fields: ['Name', 'Bio', 'Attachments'],
+        fields: ['Headline', 'Sub-headline', 'Header image'],
+        filterByFormula: "IF({Headline}, {Sub-headline}, NOT({Header image} = BLANK()))"
     }).all();
+
+    console.log(records)
     const data = records.map((item) => {
+
         return {
-            name: item.get('Name'),
-            bio: item.get('Bio'),
-            Attachments: item.get('Attachments').map((attachment) => attachment.url)
+            headline: item.get('Headline'),
+            subHeadline: item.get('Sub-headline'),
+            image: item.get('Header image') || null
         };
+
+
     });
     return {
         props: {

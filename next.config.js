c1963fb8342bc96208f4e371d57646b7c411f4f3
aya <aya.bochman@gmail.com>
2021-02-17 21:45:39 +0200

    updated packages

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
+    AIRTABLE_BASE_ID: process.env.AIRTABLE_BASE_ID,
+    AIRTABLE_TABLE_NAME: process.env.AIRTABLE_TABLE_NAME
+  }
+}
